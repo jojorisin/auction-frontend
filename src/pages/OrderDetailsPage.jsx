065d3a83ba731b31100f4601a265d4cb46c280b9
johannajonsson <jojo.risin@outlook.com>
2026-04-07 11:39:28 +0200

    return to window.location.href. add paymentLoading and redirect immediately

--- src/pages/OrderDetailsPage.jsx
+++ src/pages/OrderDetailsPage.jsx
@@ -2,22 +2,22 @@ import { getMyOrderById, createCheckoutSession } from "../services/api";
 import { useEffect, useState } from "react";
 import { Container, Alert, Table, Row, Col, Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import { loadStripe } from "@stripe/stripe-js";
 
 const OrderDetailsPage = () => {
   const { id } = useParams();
   const [order, setOrder] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [paymentLoading, setPaymentLoading] = useState(false);
 
   const handlePayment = async (orderId) => {
+    setPaymentLoading(true);
     try {
-      const stripe = await loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
       const session = await createCheckoutSession(orderId);
-      await stripe.redirectToCheckout({ sessionId: session.data.sessionId });
+      window.location.href = session.data?.url;
+      setPaymentLoading(false);
     } catch (error) {
-      console.error("Payment was unsuccessful:", error);
-      alert(`Unable to process payment: ${error.message}`);
+      alert(`Unable to process payment. ${error.message}`);
     }
   };
 
@@ -49,19 +49,6 @@ const OrderDetailsPage = () => {
         <Alert variant="danger">{error || "Hittade ingen order"}</Alert>
       </Container>
     );
-
-  const getExclVat = (amount) => Number(amount) / 1.25;
-  const total = Number(order.orderSum);
-
-  const commissionInclVat = total * 0.2;
-  const commissionExclVat = getExclVat(commissionInclVat);
-  const commissionVat = commissionInclVat - commissionExclVat;
-
-  const premiumExclVat = 50;
-  const premiumVat = 50 * 0.25;
-  const premiumInclVat = 50 * 1.25;
-
-  const finalTotal = total + commissionInclVat + premiumInclVat;
 
   // temporary fix for missing fields in backend.
   return (
@@ -150,20 +137,27 @@ const OrderDetailsPage = () => {
           <Col className="d-flex justify-content-end mt-3">
             {order.status === "PENDING" && (
               <Button
+                disabled={paymentLoading}
                 className="btn btn-dark d-flex align-items-center shadow-sm py-2 px-4"
                 onClick={() => handlePayment(order.orderId)}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="currentColor"
-                  className="bi bi-stripe me-2"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.226 5.385c-.584 0-.937.164-.937.593 0 .468.607.674 1.36.93 1.228.415 2.844.963 2.851 2.993C11.5 11.868 9.924 13 7.63 13a7.7 7.7 0 0 1-3.009-.626V9.758c.926.506 2.095.88 3.01.88.617 0 1.058-.165 1.058-.671 0-.518-.658-.755-1.453-1.041C6.026 8.49 4.5 7.94 4.5 6.11 4.5 4.165 5.988 3 8.226 3a7.3 7.3 0 0 1 2.734.505v2.583c-.838-.45-1.896-.703-2.734-.703" />
-                </svg>
-                Pay safe with Stripe
+                {paymentLoading ? (
+                  "Processing..."
+                ) : (
+                  <>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      fill="currentColor"
+                      className="bi bi-stripe me-2"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.226 5.385c-.584 0-.937.164-.937.593 0 .468.607.674 1.36.93 1.228.415 2.844.963 2.851 2.993C11.5 11.868 9.924 13 7.63 13a7.7 7.7 0 0 1-3.009-.626V9.758c.926.506 2.095.88 3.01.88.617 0 1.058-.165 1.058-.671 0-.518-.658-.755-1.453-1.041C6.026 8.49 4.5 7.94 4.5 6.11 4.5 4.165 5.988 3 8.226 3a7.3 7.3 0 0 1 2.734.505v2.583c-.838-.45-1.896-.703-2.734-.703" />
+                    </svg>
+                    Pay safe with Stripe
+                  </>
+                )}
               </Button>
             )}
           </Col>
